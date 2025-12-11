e7f5a14488aa6c1eb4dccf1177eb71969b772c9c
akashagalave <akashagalaveaaa1@gmail.com>
2025-12-11 13:40:46 +0530

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End To End Real Time Fraud Detection System.',
+    author='akash agalave',
+    license='MIT',
+)
